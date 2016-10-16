1a908425c35a8f2074cfab31464c79a9cf52346e
Jeremy Muhia <jmuhia80@gmail.com>
2016-10-16 19:24:04 -0400

    connect views

--- iOS Programming Homework 2/AddItemsViewController.m
+++ iOS Programming Homework 2/AddItemsViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "AddItemsViewController.h"
+#import "RootViewController.h"
 
 @interface AddItemsViewController ()
 
@@ -26,12 +27,21 @@
 
 /*
 #pragma mark - Navigation
+*/
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
+    
+    RootViewController * rootVC = [segue destinationViewController];
+    
+    NSLog(@"...starting segue %@", segue.identifier);
+    
+    if ([segue.identifier isEqualToString:@"addItemsAction"]) {
+    
+    }
 }
-*/
+
 
 @end
