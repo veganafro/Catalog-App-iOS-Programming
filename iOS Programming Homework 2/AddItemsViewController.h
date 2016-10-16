be9047cd749a0318b360fa54770c53714d80b1a1
Jeremy Muhia <jmuhia80@gmail.com>
2016-10-15 22:27:07 -0400

    build view controller classes

---
+++ iOS Programming Homework 2/AddItemsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddItemsViewController.h
+//  iOS Programming Homework 2
+//
+//  Created by Jeremia Muhia on 10/15/16.
+//  Copyright © 2016 NYU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddItemsViewController : UIViewController
+
+@end
---
+++ iOS Programming Homework 2/AddItemsViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddItemsViewController.m
+//  iOS Programming Homework 2
+//
+//  Created by Jeremia Muhia on 10/15/16.
+//  Copyright © 2016 NYU. All rights reserved.
+//
+
+#import "AddItemsViewController.h"
+
+@interface AddItemsViewController ()
+
+@end
+
+@implementation AddItemsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
