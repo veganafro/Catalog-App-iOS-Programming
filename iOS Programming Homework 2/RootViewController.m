f67fe180eb417d16ebfc32da0bc5aa8f95ff055f
Jeremy Muhia <jmuhia80@gmail.com>
2016-10-16 08:48:31 -0400

    add segues

--- iOS Programming Homework 2/RootViewController.m
+++ iOS Programming Homework 2/RootViewController.m
@@ -14,6 +14,11 @@
 
 @implementation RootViewController
 
+- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
