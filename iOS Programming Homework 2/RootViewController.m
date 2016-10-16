77d918798718b60576db4c715d2f7ec97bc9120b
Jeremy Muhia <jmuhia80@gmail.com>
2016-10-15 22:14:08 -0400

    build view controller classes

--- iOS Programming Homework 2/RootViewController.m
+++ iOS Programming Homework 2/RootViewController.m
@@ -6,13 +6,13 @@
 //  Copyright © 2016 NYU. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "RootViewController.h"
 
-@interface ViewController ()
+@interface RootViewController ()
 
 @end
 
-@implementation ViewController
+@implementation RootViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
